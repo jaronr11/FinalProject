6220781c4ad7a691b03b6c0d0dd6275c0008ef4d
jaronr11 <jaro8829@colorado.edu>
2025-12-04 00:48:35 -0700

    Implement facade pattern for game creation

--- src/main/java/MazeGame/controller/Game.java
+++ src/main/java/MazeGame/controller/Game.java
@@ -15,35 +15,7 @@ import java.util.List;
 
 public class Game {
     public static void main(String[] args) {
-        CharacterFactory cf =  new CharacterFactory();
-        Position pos = new Position(0,0);
-        Player p1 = cf.createPlayer(pos, 1);
-        Room roomOne = new Room();
-        Position[] positions = {new  Position(0,5),new Position(3,0),new Position(7,4)};
-        Room roomTwo = new Room(positions);
-        roomOne.connectRoom(roomTwo);
-        Maze maze = new Maze(p1,roomOne);
-        Character e1 = new Enemy(new RandomMoveStrategy(), new Position(5,5), 1.0);
-        Room room = maze.getCurrentRoom();
-        room.addEnemy(e1);
-        Character e2 = new Enemy(new FollowMove(), new Position(5,5), 10.0 );
-        roomTwo.addEnemy(e2);
-
-        Weapon weapon = new Weapon(ProjectileFactory.ProjectileType.BURST_PROJECTILE, new Position(3,5));
-        room.addWeapon(weapon);
-
-        JFrame frame = new JFrame("EXAMPLE");
-
-        GameView gv = new GameView(maze);
-        GameController gc = new GameController(maze, gv, frame);
-        gv.addKeyListener(gc);
-        gv.addMouseListener(gc);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(gv);
-        frame.setSize(500, 750);
-        frame.setVisible(true);
-        gv.requestFocusInWindow();
-
-       gc.startGame();
+        GameFacade gameFacade = new GameFacade();
+        gameFacade.startGame();
     }
 }
---
+++ src/main/java/MazeGame/controller/GameFacade.java
@@ -0,0 +1,117 @@
+package MazeGame.controller;
+
+import MazeGame.model.Door;
+import MazeGame.model.GameEntities.*;
+import MazeGame.model.GameEntities.Character;
+import MazeGame.model.Maze;
+import MazeGame.model.MovementStrategies.FollowMove;
+import MazeGame.model.MovementStrategies.RandomMoveStrategy;
+import MazeGame.model.Position;
+import MazeGame.model.Room;
+import MazeGame.view.GameView;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class GameFacade {
+
+    private Maze maze;
+    private GameView gameView;
+    private GameController gameController;
+    private JFrame frame;
+    CharacterFactory characterFactory = new CharacterFactory();
+
+
+    public void startGame() {
+        createModel();
+        createViewAndController();
+        showWindow();
+        gameController.startGame();
+    }
+
+    void createModel() {
+        Position pos = new Position(0,0);
+        Player p1 = characterFactory.createPlayer(pos, 10);
+
+        Room roomOne = buildRoomOne();
+        Room roomTwo = buildRoomTwo();
+        Room roomThree = buildRoomThree();
+        roomOne.connectRoom(roomTwo);
+        roomTwo.connectRoom(roomThree);
+
+
+        maze = new Maze(p1,roomOne);
+    }
+
+    Room buildRoomOne() {
+        Character e1 = new Enemy(new RandomMoveStrategy(), new Position(5,5), 4.0);
+        Door door = new Door(new Position(0,0));
+        return new Room.Builder().addDoor(door).addEnemy(e1).build();
+    }
+
+    Room buildRoomTwo() {
+        Character e2 = new Enemy(new RandomMoveStrategy(), new Position(7,5), 3.0 );
+
+        Position[] obstacles = {
+                new Position(2,1), new Position(3,1), new Position(4,1),
+                new Position(10,1), new Position(11,1), new Position(12,1),
+
+                new Position(2,3), new Position(3,3), new Position(4,3),
+                new Position(10,3), new Position(11,3), new Position(12,3),
+
+                new Position(2,5), new Position(3,5), new Position(4,5),
+                new Position(10,5), new Position(11,5), new Position(12,5),
+
+                new Position(2,7), new Position(3,7), new Position(4,7),
+                new Position(10,7), new Position(11,7), new Position(12,7)
+        };
+        Door door = new Door(new Position(5,5));
+        return new Room.Builder().addObstacles(obstacles).addEnemy(e2).addDoor(door).build();
+    }
+
+    Room buildRoomThree() {
+        Position[] obstacles = {
+                new Position(2,1), new Position(3,1), new Position(4,1),
+                new Position(5,1), new Position(6,1), new Position(7,1),
+                new Position(8,1), new Position(9,1), new Position(10,1),
+                new Position(11,1), new Position(12,1),
+
+                new Position(2,7), new Position(3,7), new Position(4,7),
+                new Position(5,7), new Position(6,7), new Position(7,7),
+                new Position(8,7), new Position(9,7), new Position(10,7),
+                new Position(11,7), new Position(12,7),
+
+                new Position(2,2), new Position(2,3),
+                new Position(2,5), new Position(2,6),
+
+                new Position(12,2), new Position(12,3),new Position(12,4),
+                new Position(12,5), new Position(12,6)
+        };
+        Door door = new Door(new Position(14,0));
+        Character enemyOne = characterFactory.createEnemy(new FollowMove(), new Position(8,5), 4.0);
+        List<Character> enemies = List.of(enemyOne);
+        Weapon slowWeapon = new Weapon(ProjectileFactory.ProjectileType.SLOW_PROJECTILE, new Position(9,5));
+
+        return new Room.Builder().addEnemies(enemies).addObstacles(obstacles).addDoor(door).addWeapon(slowWeapon).build();
+
+    }
+
+    void createViewAndController() {
+        frame = new JFrame("MazeGame");
+        gameView = new GameView(maze);
+        gameController = new GameController(maze, gameView, frame);
+        gameView.addKeyListener(gameController);
+        gameView.addMouseListener(gameController);
+    }
+
+    void showWindow() {
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(gameView);
+        frame.setSize(500, 750);
+        frame.setVisible(true);
+        gameView.requestFocusInWindow();
+    }
+
+}
