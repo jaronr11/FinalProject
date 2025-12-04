811e81ed5cd05c8c157333c3e9a3acced1223e17
jaronr11 <jaro8829@colorado.edu>
2025-12-04 00:49:58 -0700

    Room builder pattern implementation

--- src/main/java/MazeGame/model/Door.java
+++ src/main/java/MazeGame/model/Door.java
@@ -6,6 +6,12 @@ public class Door {
     private Position nextLocationPos;
     private boolean isOpen;
 
+    public Door(Position position) {
+        this.position = position;
+        this.nextLocation = null;
+        this.isOpen = false;
+    }
+
     public Door(Position position, Room nextLocation) {
         this.position = position;
         this.nextLocation = nextLocation;
